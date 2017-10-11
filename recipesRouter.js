64a3b55dd209b256933bdd4cde4eac6a7c41d181
pedrom40 <pedro.morin1980@gmail.com>
2017-10-11 08:25:50 -0500

    add tests for recipes endpoints

--- recipesRouter.js
+++ recipesRouter.js
@@ -7,22 +7,17 @@ const jsonParser = bodyParser.json();
 
 const {Recipes} = require('./models');
 
-// we're going to add some recipes to Recipes
-// so there's some data to look at
-Recipes.create(
-  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
-Recipes.create(
-  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+// we're going to add some recipes to Recipes so there's some data to look at
+Recipes.create('boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create('milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
-// send back JSON representation of all recipes
-// on GET requests to root
+// send back JSON representation of all recipes on GET requests to root
 router.get('/', (req, res) => {
   res.json(Recipes.get());
 });
 
 
-// when new recipe added, ensure has required fields. if not,
-// log error and return 400 status code with hepful message.
+// when new recipe added, ensure has required fields. if not, log error and return 400 status code with hepful message.
 // if okay, add new item, and return it with a status 201.
 router.post('/', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
@@ -46,10 +41,8 @@ router.delete('/:id', (req, res) => {
   res.status(204).end();
 });
 
-// when PUT request comes in with updated recipe, ensure has
-// required fields. also ensure that recipe id in url path, and
-// recipe id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
+// when PUT request comes in with updated recipe, ensure has required fields. also ensure that recipe id in url path, and
+// recipe id in updated item object match. if problems with any of that, log error and send back status code 400. otherwise
 // call `Recipes.updateItem` with updated recipe.
 router.put('/:id', jsonParser, (req, res) => {
   const requiredFields = ['name', 'ingredients', 'id'];
--- test/test-server.js
+++ test/test-server.js
@@ -3,32 +3,24 @@ const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
 
-// this lets us use *should* style syntax in our tests
-// so we can do things like `(1 + 1).should.equal(2);`
-// http://chaijs.com/api/bdd/
+// this lets us use *should* style syntax in our tests so we can do things like `(1 + 1).should.equal(2);` http://chaijs.com/api/bdd/
 const should = chai.should();
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
+// This let's us make HTTP requests in our tests. see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
 
 describe('Shopping List', function() {
 
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
+  // Before our tests run, we activate the server. Our `runServer` function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here, there's a possibility of a race condition where our tests start
   // running before our server has started.
   before(function() {
     return runServer();
   });
 
-  // although we only have one test module at the moment, we'll
-  // close our server at the end of these tests. Otherwise,
-  // if we add another test module that also has a `before` block
-  // that starts our server, it will cause an error because the
+  // although we only have one test module at the moment, we'll close our server at the end of these tests. Otherwise,
+  // if we add another test module that also has a `before` block that starts our server, it will cause an error because the
   // server would still be running from the previous tests.
   after(function() {
     return closeServer();
@@ -36,13 +28,10 @@ describe('Shopping List', function() {
 
   // test strategy:
   //   1. make request to `/shopping-list`
-  //   2. inspect response object and prove has right code and have
-  //   right keys in response object.
+  //   2. inspect response object and prove has right code and have right keys in response object.
   it('should list items on GET', function() {
-    // for Mocha tests, when we're dealing with asynchronous operations,
-    // we must either return a Promise object or else call a `done` callback
-    // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
+    // for Mocha tests, when we're dealing with asynchronous operations, we must either return a Promise object or else call a `done` callback
+    // at the end of the test. The `chai.request(server).get...` call is asynchronous and returns a Promise, so we just return it.
     return chai.request(app)
       .get('/shopping-list')
       .then(function(res) {
@@ -52,8 +41,7 @@ describe('Shopping List', function() {
 
         // because we create three items on app load
         res.body.length.should.be.at.least(1);
-        // each item should be an object with key/value pairs
-        // for `id`, `name` and `checked`.
+        // each item should be an object with key/value pairs for `id`, `name` and `checked`.
         const expectedKeys = ['id', 'name', 'checked'];
         res.body.forEach(function(item) {
           item.should.be.a('object');
@@ -64,8 +52,7 @@ describe('Shopping List', function() {
 
   // test strategy:
   //  1. make a POST request with data for a new item
-  //  2. inspect response object and prove it has right
-  //  status code and that the returned object has an `id`
+  //  2. inspect response object and prove it has right status code and that the returned object has an `id`
   it('should add an item on POST', function() {
     const newItem = {name: 'coffee', checked: false};
     return chai.request(app)
@@ -88,12 +75,9 @@ describe('Shopping List', function() {
   //  2. make a GET request so we can get an item to update
   //  3. add the `id` to `updateData`
   //  4. Make a PUT request with `updateData`
-  //  5. Inspect the response object to ensure it
-  //  has right status code and that we get back an updated
-  //  item with the right data in it.
+  //  5. Inspect the response object to ensure it has right status code and that we get back an updated item with the right data in it.
   it('should update items on PUT', function() {
-    // we initialize our updateData here and then after the initial
-    // request to the app, we update it with an `id` property so
+    // we initialize our updateData here and then after the initial request to the app, we update it with an `id` property so
     // we can make a second, PUT call to the app.
     const updateData = {
       name: 'foo',
@@ -105,10 +89,8 @@ describe('Shopping List', function() {
       .get('/shopping-list')
       .then(function(res) {
         updateData.id = res.body[0].id;
-        // this will return a promise whose value will be the response
-        // object, which we can inspect in the next `then` back. Note
-        // that we could have used a nested callback here instead of
-        // returning a promise and chaining with `then`, but we find
+        // this will return a promise whose value will be the response object, which we can inspect in the next `then` back. Note
+        // that we could have used a nested callback here instead of returning a promise and chaining with `then`, but we find
         // this approach cleaner and easier to read and reason about.
         return chai.request(app)
           .put(`/shopping-list/${updateData.id}`)
@@ -121,13 +103,11 @@ describe('Shopping List', function() {
   });
 
   // test strategy:
-  //  1. GET a shopping list items so we can get ID of one
-  //  to delete.
+  //  1. GET a shopping list items so we can get ID of one to delete.
   //  2. DELETE an item and ensure we get back a status 204
   it('should delete items on DELETE', function() {
     return chai.request(app)
-      // first have to get so we have an `id` of item
-      // to delete
+      // first have to get so we have an `id` of item to delete
       .get('/shopping-list')
       .then(function(res) {
         return chai.request(app)
@@ -137,4 +117,99 @@ describe('Shopping List', function() {
         res.should.have.status(204);
       });
   });
+
+
+  // test strategy:
+  //   1. make request to `/recipes`
+  //   2. inspect response object and prove has right code and have right keys in response object.
+  it('should list recipes on GET', function() {
+    
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+
+        // because we create three items on app load
+        res.body.length.should.be.at.least(1);
+        // each item should be an object with key/value pairs for `id`, `name` and `ingredients`.
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+
+      });
+
+  });
+
+  // test strategy:
+  //  1. make a POST request with data for a new item
+  //  2. inspect response object and prove it has right status code and that the returned object has an `id`
+  it('should add a recipe on POST', function() {
+    const newItem = {name: 'toast', ingredients: ['bread', 'butter']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.id.should.not.be.null;
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+  // test strategy:
+  //  1. initialize some update data (we won't have an `id` yet)
+  //  2. make a GET request so we can get an item to update
+  //  3. add the `id` to `updateData`
+  //  4. Make a PUT request with `updateData`
+  //  5. Inspect the response object to ensure it has right status code and that we get back an updated item with the right data in it.
+  it('should update items on PUT', function() {
+    // we initialize our updateData here and then after the initial request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      name: 'toast',
+      ingredients: ['bread', 'butter']
+    };
+
+    return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        // this will return a promise whose value will be the response object, which we can inspect in the next `then` back. Note
+        // that we could have used a nested callback here instead of returning a promise and chaining with `then`, but we find
+        // this approach cleaner and easier to read and reason about.
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      // prove that the PUT request has right status code
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+  // test strategy:
+  //  1. GET a recipe so we can get ID of one to delete.
+  //  2. DELETE an item and ensure we get back a status 204
+  it('should delete a recipe on DELETE', function() {
+    return chai.request(app)
+      // first have to get so we have an `id` of item to delete
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
 });
